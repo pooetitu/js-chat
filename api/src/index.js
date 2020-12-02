8fab5db992e31688cd119c8b6a2feff1c817ea39
pooetitu <pooetitu@gmail.com>
2020-12-02 21:00:02 +0100

    Add broadcast

--- api/src/index.js
+++ api/src/index.js
@@ -1,20 +1,26 @@
-const express = require('express'),
-    ws = require('ws'),
-    http = require('http'),
-    app = express();
+const express = require("express"),
+  ws = require("ws"),
+  http = require("http"),
+  app = express();
 // use express static to deliver resources HTML, CSS, JS, etc)
 // from the public folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 var httpServer = http.createServer(null, app).listen(3000);
 console.log("The HTTP server is up and running");
 
-var wss = new ws.Server({server: httpServer});
+var wss = new ws.Server({ server: httpServer });
+var clientList = [];
 
-wss.on('connection', function (client) {
-    client.on('message', function (event) {
-        a = JSON.parse(event);
-        console.log(a.text);
+wss.on("connection", function (client) {
+  clientList.push(client);
+  client.on("message", function (event) {
+    message = JSON.parse(event);
+    wss.broadcast = () => {
+      clientList.forEach(function (client) {
+        client.send(message);
+      });
+    };
+    console.log(message.text);
+  });
+  console.log("A new client was connected.");
 });
-    console.log("A new client was connected.");
-});
-
