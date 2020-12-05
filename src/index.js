500698f752f82fc2dc39745cc2bf04e02632080f
pooetitu <pooetitu@gmail.com>
2020-12-05 15:38:57 +0100

    Add front room list

--- src/index.js
+++ src/index.js
@@ -6,12 +6,18 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + "\\app\\index.html");
 });
 
+let rooms = new Map();
+
 io.on("connection", (socket) => {
   console.log("User connected");
+  socket.emit("rooms list", rooms.keys);
   socket.on("message", (data) => {
     console.log(data);
     socket.broadcast.emit("message", data);
   });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  })
 });
 
 http.listen(3000, () => {
